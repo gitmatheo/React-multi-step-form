98886d7a72b8326da45cfbcddc7aba153c292212
Mateusz <matteuszd@gmail.com>
2019-02-18 09:25:19 +0100

    first commit

---
+++ src/components/Confirm.js
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import AppBar from "material-ui/AppBar";
+import { List, ListItem } from "material-ui/List";
+import RaisedButton from "material-ui/RaisedButton";
+
+export class Confirm extends Component {
+  continue = e => {
+    e.preventDefault();
+    // PROCESS FORM //
+
+    this.props.nextStep();
+  };
+
+  back = e => {
+    e.preventDefault();
+    this.props.prevStep();
+  };
+
+  confirmation = e => {
+    e.preventDefault();
+    this.props.confirmation();
+  };
+  render() {
+    const {
+      values: { firstName, lastName, email, occupation, city, bio }
+    } = this.props;
+
+    return (
+      <MuiThemeProvider>
+        <React.Fragment>
+          <AppBar title="Confirm" />
+          <List>
+            <ListItem primaryText="First Name" secondaryText={firstName} />
+            <ListItem primaryText="Last Name" secondaryText={lastName} />
+            <ListItem primaryText="Email" secondaryText={email} />
+            <ListItem primaryText="Occupation" secondaryText={occupation} />
+            <ListItem primaryText="City" secondaryText={city} />
+            <ListItem primaryText="Bio" secondaryText={bio} />
+          </List>
+          <br />
+          <RaisedButton
+            label="Confirm & Continue"
+            primary={true}
+            style={styles.button}
+            onClick={this.continue}
+          />
+          <RaisedButton
+            label="Back"
+            primary={false}
+            style={styles.button}
+            onClick={this.back}
+          />
+        </React.Fragment>
+      </MuiThemeProvider>
+    );
+  }
+}
+
+const styles = {
+  button: {
+    margin: 15
+  }
+};
+
+export default Confirm;
---
+++ src/components/UserForm.js
@@ -0,0 +1,77 @@
+import React, { Component } from "react";
+import FormUserDetails from "./FormUserDetails";
+import FormPersonalDetails from "./FormPersonalDetails";
+import Confirm from "./Confirm";
+import Success from "./Success";
+
+export class UserForm extends Component {
+  state = {
+    step: 1,
+    firstName: "",
+    lastName: "",
+    email: "",
+    occupation: "",
+    city: "",
+    bio: ""
+  };
+
+  //Proceed to next step
+  nextStep = () => {
+    const { step } = this.state;
+    this.setState({
+      step: step + 1
+    });
+  };
+
+  //Proceed to previous step
+  prevStep = () => {
+    const { step } = this.state;
+    this.setState({
+      step: step - 1
+    });
+  };
+
+  //Handle fields change
+  handleChange = input => e => {
+    this.setState({ [input]: e.target.value });
+  };
+
+  render() {
+    const { step } = this.state; // pull out the step out of the state
+    const { firstName, lastName, email, occupation, city, bio } = this.state; // pull out all the field
+    const values = { firstName, lastName, email, occupation, city, bio }; // pass the value into each component
+
+    switch (step) {
+      case 1:
+        return (
+          <FormUserDetails
+            nextStep={this.nextStep}
+            handleChange={this.handleChange}
+            values={values}
+          />
+        );
+      case 2:
+        return (
+          <FormPersonalDetails
+            nextStep={this.nextStep}
+            prevStep={this.prevStep}
+            handleChange={this.handleChange}
+            values={values}
+          />
+        );
+      case 3:
+        return (
+          <Confirm
+            nextStep={this.nextStep}
+            prevStep={this.prevStep}
+            values={values}
+          />
+        );
+      case 4:
+        return <Success />;
+    }
+    return <div />;
+  }
+}
+
+export default UserForm;
